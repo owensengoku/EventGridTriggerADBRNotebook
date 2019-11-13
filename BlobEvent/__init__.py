3d6c62fbadf1f1c760ea8bb51649ac837a03d400
Will Johnson <wijohns@microsoft.com>
2019-11-12 22:43:55 -0600

    Initial commit

---
+++ BlobEvent/__init__.py
@@ -0,0 +1,42 @@
+import json
+import logging
+
+from .dbr import run_job
+
+import azure.functions as func
+
+def main(event: func.EventGridEvent):
+    result = json.dumps({
+        'id': event.id,
+        'data': event.get_json(),
+        'topic': event.topic,
+        'subject': event.subject,
+        'event_type': event.event_type,
+    })
+
+    logging.info('Python EventGrid trigger received an event: %s', result)
+
+    blob_url = event.get_json().get('data',{}).get("url")
+    if blob_url is None:
+        logging.info(f"{event.id} did not contain a 'url' attribute in its 'data' attribute.  Exiting.")
+        return None
+    
+    # Reshape the blob url to be wasbs format (wasbs://CONTAINER@STORAGE_ACCOUNT/FILE_PATH)
+    # Input as https://STORAGE_ACCOUNT/CONTAINER/FILE_PATH
+    slashes_pos = blob_url.index('://') + 3
+    end_storage_acct_pos = blob_url[(slashes_pos):].index('/') + slashes_pos
+    end_container_pos = blob_url[(end_storage_acct_pos+1):].index('/')+end_storage_acct_pos+1
+
+    storage_acct = blob_url[slashes_pos:end_storage_acct_pos]
+    container = blob_url[(end_storage_acct_pos+1):end_container_pos]
+    filepath = blob_url[(end_container_pos+1):]
+
+    wasbs_url = f"wasbs://{container}@{storage_acct}/{filepath}"
+    logging.info(f"Running a job for this wasbs file: {wasbs_url}")
+    job_results = json.dumps(run_job(wasbs_url),indent = 2)
+
+    logging.info(f"Job trigger results were: {job_results}")
+    logging.info(f"Completed Event Grid Trigger for {event.id}")
+
+
+    
---
+++ BlobEvent/dbr.py
@@ -0,0 +1,26 @@
+import os
+import requests
+
+
+_DBR_TOKEN = os.getenv("DBR_TOKEN")
+_DBR_WORKSPACE = os.getenv("DBR_TOKEN")
+_DBR_JOB_ID = os.getenv("DBR_JOB_ID")
+
+def auth():
+    return {"Authorization": f"Bearer {_DBR_TOKEN}"}
+
+def run_job(blob_url):
+    job_url = _DBR_WORKSPACE + '/api/2.0/jobs/run-now'
+    payload = {
+        "job_id":_DBR_JOB_ID,
+        "notebook_params":{
+            "blob_url": blob_url
+        }
+    }
+    results = requests.post(
+        url= job_url,
+        data= payload,
+        headers = auth()
+    )
+
+    return results.json()
